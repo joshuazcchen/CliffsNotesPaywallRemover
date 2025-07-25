0e2b8e4f699a8a8721dfefcc3efd5bae4b486ec8
Joshua Chen <joshuaz.chen@mail.utoronto.ca>
2025-07-25 13:22:17 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,9 @@
+function removePaywall() {
+  document.querySelectorAll("div.tw-py-16.tw-px-32.tw-bg-black.tw-text-white").forEach(el => el.remove());
+  document.querySelectorAll("div.tw-flex.tw-justify-center.tw-items-center.tw-absolute.tw-top-0.tw-z-20").forEach(el => el.remove())
+  document.querySelectorAll(".tw-backdrop-blur-sm").forEach(el => {
+    el.classList.remove("tw-backdrop-blur-sm");
+  });
+}
+
+removePaywall();
